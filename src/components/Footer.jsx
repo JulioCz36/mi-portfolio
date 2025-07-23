21ff864972a09fd5fe75dfcfacf72b8448edebfd
JulioCz36 <rodriguez.julio.cesar.322@gmail.com>
2025-07-23 20:39:53 -0300

    ruta para produccion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,14 @@
 import './Footer.css'; 
 import { useTranslation } from "react-i18next";
 
+
+import github from '../assets/networks/github.png';
+import twitter from '../assets/networks/twitter.png';
+import linkedin from '../assets/networks/linkedin.png';
+import youtube from '../assets/networks/youtube.png';
+import itch from '../assets/networks/itch.png';
+
+
 const Footer = () => {
   const { t} = useTranslation();
 
@@ -9,19 +17,19 @@ const Footer = () => {
     <div class="container">
       <div class="social-links">
         <a href="https://github.com/JulioCz36" class="social-icon" target="_blank">
-          <img src="./src/assets/networks/github.png" alt="GitHub"/>
+          <img src={github} alt="GitHub"/>
         </a>
         <a href="https://x.com/JulioCzPrograma" class="social-icon" target="_blank">
-          <img src="./src/assets/networks/twitter.png" alt="x"/>
+          <img src={twitter} alt="x"/>
         </a>
         <a href="https://www.linkedin.com/in/julio-c%C3%A9sar-rodriguez-346130248/" class="social-icon" target="_blank">
-          <img src="./src/assets/networks/linkedin.png" alt="linkedin"/>
+          <img src={linkedin} alt="linkedin"/>
         </a>
         <a href="https://www.youtube.com/@JulioCz_18" class="social-icon" target="_blank">
-          <img src="./src/assets/networks/youtube.png" alt="youtube" />
+          <img src={youtube} alt="youtube" />
         </a>
          <a href="https://juliocz36.itch.io/" class="social-icon" target="_blank">
-          <img src="./src/assets/networks/itch.png" alt="itch"/>
+          <img src={itch} alt="itch"/>
         </a>
       </div>
       <div class="copyright">
