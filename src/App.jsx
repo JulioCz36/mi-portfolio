c79fad33535b22551405e44343267eff528b9dab
JulioCz36 <rodriguez.julio.cesar.322@gmail.com>
2025-07-23 19:54:28 -0300

    mi portfolio web 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Header from './components/header'
+import Header from './components/Header'
 import VideoLoop from './components/VideoLoop';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Contact from './components/contact';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './App.css'
 
